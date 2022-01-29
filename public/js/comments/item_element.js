c7e1fba3b8c249bdeda6fd05f3a8fa516b9d9843
Simon Schrape <sim@ilitu.de>
2022-01-29 10:52:12 +0100

    [FE] Style comments elements.

--- public/js/comments/item_element.js
+++ public/js/comments/item_element.js
@@ -5,6 +5,24 @@ export default class ItemElement extends HTMLElement {
     super();
     this.attachShadow({ mode: "open" });
     this.shadowRoot.innerHTML = `
+      <style>
+        :host {
+          display: block;
+        }
+        header, footer {
+          margin: 0.5rem 0;
+        }
+        md-div {
+          display: block;
+          background: var(--base01);
+          padding: 0 1rem;
+          margin: 0.5rem 0;
+          border-radius: 0.125rem;
+          max-height: 23em;
+          overflow: auto;
+        }
+      </style>
+
       <header>
         <strong id="name-field"></strong>
         <small><simple-date id="date-field"></simple-date></small>
--- public/js/comments/list_element.js
+++ public/js/comments/list_element.js
@@ -5,6 +5,27 @@ customElements.define("comments-item", ItemElement);
 export default class ListElement extends HTMLElement {
   constructor() {
     super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = `<style>
+      :host {
+        display: block;
+      }
+      ol {
+        /* counter-reset: cntr; */
+        margin: 0;
+        padding: 0 0 0 2rem;
+      }
+      li {
+        /* counter-increment: cntr; */
+        margin: 0.5rem 0;
+      }
+      li::marker {
+        /* content: counter(cntr) ". "; */
+        /* content: attr(cntr); */
+        color: var(--base04);
+        font-weight: bold;
+      }
+    </style>`;
   }
 
   set data(comments) {
@@ -16,6 +37,6 @@ export default class ListElement extends HTMLElement {
       li.appendChild(ci);
       ol.appendChild(li);
     }
-    this.appendChild(ol);
+    this.shadowRoot.appendChild(ol);
   }
 }
