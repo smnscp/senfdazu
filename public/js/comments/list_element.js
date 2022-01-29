976f2cc997e3c59f8b39ea57cb6e80acb24cd50c
Simon Schrape <sim@ilitu.de>
2022-01-29 22:45:36 +0100

    [FE] Remove counter styles.

--- public/js/comments/list_element.js
+++ public/js/comments/list_element.js
@@ -3,22 +3,7 @@ import ItemElement from "./item_element.js";
 customElements.define("comments-item", ItemElement);
 
 export default class ListElement extends HTMLElement {
-  static stylesheet = `<style>
-    comments-list ol {
-      counter-reset: comments-item;
-    }
-    comments-list li {
-      counter-increment: comments-item;
-    }
-    comments-list li::marker {
-      content: counters(comments-item, ".") ". ";
-      color: var(--base04);
-      font-weight: bold;
-    }
-  </style>`;
-
   set data(comments) {
-    this.innerHTML = ListElement.stylesheet;
     const ol = document.createElement("ol");
     for (var comment of comments) {
       const li = document.createElement("li");
